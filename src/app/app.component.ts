24b7a1c7f326d6e4fd2122d2f70159731ffbe117
Ravuthz <ravuthz@gmail.com>
2017-09-09 01:19:01 +0700

    Add Material to Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
+import "rxjs/add/operator/map";
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    baseUrl = "https://jsonplaceholder.typicode.com";
+    title = 'app';
+    data: Array<any>;
+    posts: Array<any>;
+
+    constructor(private http: Http) {
+        this.loadPost();
+        this.loadPhotos();
+    }
+
+    loadPost() {
+        this.http.get(this.baseUrl + '/posts')
+            .map(response => response.json())
+            .subscribe(res => this.posts = res);
+    }
+
+    loadPhotos() {
+        this.http.get(this.baseUrl + '/photos')
+            .map(response => response.json())
+            .subscribe(res => this.data = res);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        MaterialModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
